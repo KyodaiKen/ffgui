ed7fdb98ba466e96bca953a77897668824e3ddf0
Kyoudai Ken <bloodysword@gmx.net>
2026-02-08 01:01:16 +0100

    Improved publishing again

--- publish-linux.sh
+++ publish-linux.sh
@@ -6,8 +6,3 @@ dotnet publish -c Release -r linux-x64 -p:SelfContained=false -p:PublishSingleFi
 
 echo "--- 2. Copying essentials ---"
 rsync -av --exclude='ffmpeg' ./codecs/ ./publish-linux/codecs/
-cp -r ./templates ./publish-linux/templates
-
-#echo "--- 3. Pack deployment ---"
-#tar cf - ./publish-linux | xz -vv -T16 -e --lzma2=preset=9,dict=256MiB,lc=3,lp=0,pb=2,mode=normal,nice=273,mf=bt4,depth=512 > /mnt/nas/install/ffgui-linux-x64.tar.xz
-
--- zip-publish-win.sh
+++ zip-publish-win.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
-PUBLISH_DIR="./publish-win"
+TARGET_EXE="/mnt/nas/install/ffgui-win-x64-installer.exe"
+PUBLISH_DIR="/mnt/data/GIT/ffgui/publish-win"
+SFX_PATH="/mnt/data/GIT/ffgui/7z-win.sfx"
 
-7z a -sfx7z-win.sfx -t7z -m0=lzma2 -mx=9 -md=256m -mfb=64 -ms=on -mqs=on -mmt=on -sccUTF-8 "/mnt/nas/install/ffgui-win-x64-installer.exe" "$PUBLISH_DIR/*"
+7z a -sfx"$SFX_PATH" -t7z -m0=lzma2 -mx=9 -md=256m -mfb=64 -ms=on -mqs=on -mmt=on -sccUTF-8 \
+     "$TARGET_EXE" \
+     -ir!"$PUBLISH_DIR/*"
