f048d215eac087489f364758cf2e9b0870bb33cc
Kyoudai Ken <bloodysword@gmx.net>
2026-02-08 02:57:24 +0100

    Fixed the linux setup script for the icon

--- setup.sh
+++ setup.sh
@@ -6,7 +6,6 @@ APP_ID="de.kyo.ffgui"
 BINARY_NAME="ffgui"
 INSTALL_DIR="/opt/$BINARY_NAME"
 DESKTOP_FILE="/usr/share/applications/$APP_ID.desktop"
-DICON_FILE="/usr/share/applications/$APP_ID.svg"
 ICON_DIR="/usr/share/icons/hicolor/scalable/apps"
 ICON_NAME="$APP_ID.svg"
 
@@ -52,18 +51,10 @@ install_app() {
     chmod +x "$INSTALL_DIR/$BINARY_NAME"
 
     # 4. Handle Icon
-    if [ -f "$INSTALL_DIR/$ICON_NAME" ]; then
-        cp -f "$INSTALL_DIR/$ICON_NAME" "$ICON_DIR/"
-        # Ensure the icon is readable by all users
-        chmod 644 "$ICON_DIR/$ICON_NAME"
-    fi
 
-    # 5. Generate Desktop Entry
-    msg "Generating desktop entry..."
+    rm -f "$ICON_DIR/$ICON_NAME"
 
-    rm -f "$DICON_FILE"
-
-    cat <<EOF > "$DICON_FILE"
+    cat <<EOF > "$ICON_DIR/$ICON_NAME"
 <?xml version="1.0" encoding="UTF-8"?>
 <svg width="128" height="128" version="1.1" viewBox="0 0 64 64" xmlns="http://www.w3.org/2000/svg">
  <defs>
@@ -76,6 +67,15 @@ install_app() {
  <path d="m3.8139 63.217h-3.3487v-20.501h8.2722v3.9623h-4.9234v4.6514h4.5646v3.9623h-4.5646zm10.844 0h-3.3487v-20.501h8.2722v3.9623h-4.9234v4.6514h4.5646v3.9623h-4.5646zm13.036-11.743h5.8802v10.825q-1.1162 0.54553-2.4318 0.89008-1.2956 0.31584-2.8305 0.31584-3.1494 0-4.9434-2.6415-1.794-2.6702-1.794-7.9246 0-3.2158 0.85712-5.5702 0.85712-2.3831 2.5115-3.6465 1.6744-1.2921 4.0863-1.2921 1.2159 0 2.3521 0.37326 1.1561 0.34455 2.0332 0.89008l-1.0963 3.8762q-0.69765-0.48811-1.5348-0.77523-0.83718-0.31584-1.7541-0.31584-1.2956 0-2.1926 0.86137-0.87705 0.83266-1.3156 2.3257-0.43852 1.4643-0.43852 3.3593 0 2.9574 0.83718 4.7088 0.85712 1.7227 2.6511 1.7227 0.49832 0 0.95678-0.08613t0.79732-0.17227v-3.79h-2.6311zm20.85 4.0772q0 2.3544-0.67772 4.1346-0.65779 1.7802-1.9933 2.8138-1.3156 1.0049-3.3487 1.0049-2.8703 0-4.3852-2.1247t-1.5149-5.7999v-12.863h3.4085v12.318q0 2.3544 0.63786 3.3593 0.65779 1.0049 1.9335 1.0049 1.3355 0 1.9335-1.0336 0.61792-1.0624 0.61792-3.3593v-12.289h3.3886zm9.1492 7.6662h-6.9167v-2.7851l1.7541-1.0624v-12.806l-1.7541-1.0624v-2.7851h6.9167v2.7851l-1.7541 1.0624v12.806l1.7541 1.0624z" fill="#2c004e" stroke="#a0f" stroke-width=".93047" aria-label="FFGUI"/>
 </svg>
 EOF
+
+    if [ -f "$INSTALL_DIR/$ICON_NAME" ]; then
+        cp -f "$INSTALL_DIR/$ICON_NAME" "$ICON_DIR/"
+        # Making sure the icon is readable by all users
+        chmod 644 "$ICON_DIR/$ICON_NAME"
+    fi
+
+    # 5. Generate Desktop Entry
+    msg "Generating desktop entry..."
 
     rm -f "$DESKTOP_FILE"
 
